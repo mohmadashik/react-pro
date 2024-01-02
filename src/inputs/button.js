bee1f9dbef9f33e36a4f97b5cce827037f8b1e75
Mohmad Ashik  M A <73482667+mohmadashik@users.noreply.github.com>
2024-01-02 05:52:28 +0530

    manual upload without node modules

---
+++ src/inputs/button.js
@@ -0,0 +1,18 @@
+import './inputs.css';
+
+function Button() {
+  return (
+    <div className="Button">
+      <header className="Button-header">
+        <p>
+            The below is a button component
+        </p>
+        <button>
+            HELLO WORLD BUTTON!! 
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Button;
